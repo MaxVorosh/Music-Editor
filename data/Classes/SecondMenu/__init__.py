2981907828c7a5243fa654221da1d7e94f9e13a6
Макс <ma_voroshilov@mail.ru>
2020-06-19 18:51:17 +0300

    Обработка нажатия клавиш в меню мелодий

--- data/Classes/SecondMenu/__init__.py
+++ data/Classes/SecondMenu/__init__.py
@@ -38,17 +38,17 @@ class SecondMenu(Window):
             if i % 2 == 0:
                 btn = Button(self, "data\\Sprites\\old.png")
                 btn.resize(256, 160)
-                btn.move(10, 100 + i // 2 * 170)
+                btn.move(10, 100 + i // 2 * 185)
                 btn.set_func(self.go_to_melody)
             else:
                 btn = Button(self, "data\\Sprites\\old.png")
                 btn.resize(256, 160)
-                btn.move(375, 100 + i // 2 * 170)
+                btn.move(375, 100 + i // 2 * 185)
                 btn.set_func(self.go_to_melody)
             self.melodies.append(btn)
         add = Button(self, "data\\Sprites\\add_1.png")
         add.resize(256, 160)
-        add.move(10 + self.length % 2 * 365, 100 + self.length // 2 * 170)
+        add.move(10 + self.length % 2 * 365, 100 + self.length // 2 * 185)
         add.set_func(self.go_next)
         self.melodies.append(add)
         self.set_background('data\\Sprites\\bg.jpg')
@@ -68,6 +68,7 @@ class SecondMenu(Window):
                 self.go_next()
 
     def run(self):
+        start = 0
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -75,17 +76,33 @@ class SecondMenu(Window):
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 1:
                         self.click(event.pos)
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP and start != 0:
+                        start -= 2
+                        for btn in self.melodies:
+                            if btn.rect.y == -185:
+                                btn.rect.y = 100
+                            else:
+                                btn.rect.y += 185
+                    if event.key == pygame.K_DOWN and start <= self.length - 6:
+                        start += 2
+                        for btn in self.melodies:
+                            if btn.rect.y == 100:
+                                btn.rect.y = -185
+                            else:
+                                btn.rect.y -= 185
+
             self.screen.fill((0, 0, 0))
             if self.background:
                 self.screen.blit(self.background, (0, 0))
             self.sprites.draw(self.screen)
-            for i in range(self.length):
+            for i in range(start, self.length):
                 if i % 2 == 0:
-                    make_fon_by_rect(self.screen, self.names[i].split('\n'), 10, 266, 100 + i // 2 * 170,
-                                     100 + i // 2 * 170 + 160)
+                    make_fon_by_rect(self.screen, self.names[i].split('\n'), 10, 266, 100 + (i - start) // 2 * 185,
+                                     100 + (i - start) // 2 * 185 + 160)
                 else:
-                    make_fon_by_rect(self.screen, self.names[i].split('\n'), 375, 631, 100 + i // 2 * 170,
-                                     100 + i // 2 * 170 + 160)
+                    make_fon_by_rect(self.screen, self.names[i].split('\n'), 375, 631, 100 + (i - start) // 2 * 185,
+                                     100 + (i - start) // 2 * 185 + 160)
             pygame.display.flip()
 
     def exitFunc(self):
