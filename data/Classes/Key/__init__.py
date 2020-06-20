3308b50d39b7306778aa17a469ffe9385f691708
Макс <ma_voroshilov@mail.ru>
2020-06-20 12:21:26 +0300

    Ключи на нотном стане - улучшение

--- data/Classes/Key/__init__.py
+++ data/Classes/Key/__init__.py
@@ -6,6 +6,7 @@ from data.Functions.load import load
 class Key(Button):
     def __init__(self, window, is_violin, x, y):
         self.is_violin = is_violin
+        self.window = window
         if is_violin:
             super().__init__(window, "data\\Sprites\\violin_black.png")
         else:
@@ -14,19 +15,26 @@ class Key(Button):
         self.x = x
         if not is_violin:
             self.x += 10
-        self.resize(60, 60)
+            self.resize(60, 60)
+        else:
+            self.y -= 15
+            self.resize(90, 90)
         self.move(self.x, self.y)
         self.func = self.update
 
     def update(self):
-        self.is_violin = not self.is_violin
-        if self.is_violin:
-            self.set_image("data\\Sprites\\violin_black.png")
-        else:
-            self.set_image("data\\Sprites\\bass_black.png")
-        self.resize(60, 60)
-        if not self.is_violin:
-            self.x += 10
-        else:
-            self.x -= 10
-        self.move(self.x, self.y)
+        for key in self.window.keys:
+            key.is_violin = not key.is_violin
+            if key.is_violin:
+                key.set_image("data\\Sprites\\violin_black.png")
+            else:
+                key.set_image("data\\Sprites\\bass_black.png")
+            if not key.is_violin:
+                key.x += 10
+                key.y += 15
+                key.resize(60, 60)
+            else:
+                key.x -= 10
+                key.y -= 15
+                key.resize(90, 90)
+            key.move(key.x, key.y)
