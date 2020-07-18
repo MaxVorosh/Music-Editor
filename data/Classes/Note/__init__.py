914011defa296ad72d7d5139a1ba76435b4fc0f0
Макс <ma_voroshilov@mail.ru>
2020-07-18 12:29:15 +0300

    Смена штиля и секретный файл

--- data/Classes/Note/__init__.py
+++ data/Classes/Note/__init__.py
@@ -5,7 +5,13 @@ from data.Functions.load import load
 class Note(pygame.sprite.Sprite):
     def __init__(self, image, x, y, size):
         super().__init__()
-        self.image = pygame.transform.scale(pygame.image.load("data\\Sprites\\" + image + ".png"), size)
+        if y > 155:
+            self.image = pygame.transform.scale(pygame.image.load("data\\Sprites\\" + image + ".png"), size)
+        else:
+            self.image = pygame.transform.scale(pygame.image.load("data\\Sprites\\" + image + "_down.png"), size)
         self.rect = self.image.get_rect()
         self.rect.x = x
-        self.rect.y = y - size[1] // 2
+        if y > 155:
+            self.rect.y = y - size[1] // 2
+        else:
+            self.rect.y = y + size[1] // 2 - 12
